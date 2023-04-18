8e56cf5f4b15759d359c619486916b1419acac91
NebelNidas <nebelnidas@gmail.com>
2023-04-18 21:01:30 +0200

    Use SLF4J and Tinylog for logging

--- silence-remover-core/src/main/java/silenceremover/Main.java
+++ silence-remover-core/src/main/java/silenceremover/Main.java
@@ -1,7 +1,12 @@
 package silenceremover;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Silence Remover");
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		LOGGER.info("Application started");
 	}
 }
