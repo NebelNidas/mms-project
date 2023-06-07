fabe78848603a46e9c4d29b6f202d13b45e164a4
nkobsr <nikolas@obser.io>
2023-06-07 14:56:43 +0200

    Styling

---
+++ silence-remover-frontend/src/components/Header.svelte
@@ -0,0 +1,23 @@
+<style>
+	header {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		min-height: 28vh;
+	}
+
+	h1 {
+		font-size: 70px;
+		font-weight: 600;
+		color: transparent;
+		background-clip: text;
+		-webkit-background-clip: text;
+		background-image: conic-gradient(#5f27cd, #00c2cb, #273dcd);
+		text-align: center;
+	}
+</style>
+
+<header>
+	<h1>Silence Remover</h1>
+</header>
---
+++ silence-remover-frontend/src/components/Navigation.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+	import { faGithub } from '@fortawesome/free-brands-svg-icons';
+</script>
+
+<style>
+	a {
+		text-decoration: none;
+		color: #5f27cd;
+		font-size: 2em;
+	}
+
+	nav {
+		position: absolute;
+		right: 0;
+		top: 0;
+		padding: 1rem;
+	}
+</style>
+
+<nav>
+	<a href="https://github.com/NebelNidas/silence-remover">
+		<FontAwesomeIcon icon={faGithub} />
+	</a>
+</nav>
