9a104ed0582e299f6d309a574ebebc02f25edef7
nkobsr <nikolas@obser.io>
2023-06-07 14:58:06 +0200

    Added the core to the upload route

--- silence-remover-server/src/main/java/silenceremover/server/UploadController.java
+++ silence-remover-server/src/main/java/silenceremover/server/UploadController.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
@@ -16,37 +19,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import silenceremover.SilenceRemover;
+import silenceremover.config.ProjectConfig;
+
 @RestController
 public class UploadController {
 	@Value("${server.storage_path}")
 	private String fileStoragePath;
 
 	@PostMapping("/upload")
-	public ResponseEntity<InputStreamResource> handlePostRequest(@RequestParam("file") MultipartFile file, @RequestParam("minSegmentLength") int minSegmentLength, @RequestParam("maxVolume") int maxVolume, @RequestParam("targetSpeed") int targetSpeed, @RequestParam("audioOnly") boolean audioOnly) {
-		// todo validate input
-		Path path = Paths.get(fileStoragePath + File.separator + file.getOriginalFilename());
+	public ResponseEntity<InputStreamResource> handlePostRequest(@RequestParam("file") MultipartFile file, @RequestParam("minSegmentLength") float minSegmentLength, @RequestParam("maxVolume") float maxVolume, @RequestParam("targetSpeed") float targetSpeed, @RequestParam("silenceTimeThreshold") float silenceTimeThreshold) {
+		// todo where do we validate input?
 
+		Path inPath, outPath;
 		try {
-			Files.createDirectories(path.getParent());
-			Files.write(path, file.getBytes());
-		} catch (IOException e) {
+			byte[] bytes = file.getBytes();
+			String name = createFilename(bytes, "mp4");
+			inPath = Paths.get(fileStoragePath + File.separator + "in" + File.separator + name);
+			outPath = Paths.get(fileStoragePath + File.separator + "out" + File.separator + name);
+			Files.write(inPath, bytes);
+		} catch (IOException | NoSuchAlgorithmException e) {
 			// todo handle error
 			e.printStackTrace();
 
 			return ResponseEntity.internalServerError().body(null);
 		}
 
-		// todo process
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
 
-		String processedPathName = fileStoragePath + File.separator + file.getOriginalFilename();
+		ProjectConfig config = ProjectConfig.builder(inPath, outPath)
+			.minSegmentMillis((int) minSegmentLength)
+			.maxVolume((int) maxVolume)
+			.targetSpeed(targetSpeed)
+			.build();
 
-		// todo really join the file path
-		File processedFile = new File(processedPathName);
+		SilenceRemover sr = new SilenceRemover();
+		sr.process(config, null);
+
+		File processedFile = new File(outPath.toString());
 
 		try {
 			InputStreamResource resource = new InputStreamResource(new FileInputStream(processedFile));
@@ -63,5 +72,20 @@ public class UploadController {
 		} finally {
 			// todo remove the processed file after download
 		}
+	}
+
+	private String createFilename(byte[] bytes, String extension) throws NoSuchAlgorithmException {
+		return String.format("%s-%d.%s", getChecksum(bytes), Instant.now().getEpochSecond(), extension);
+	}
+
+	private String getChecksum(byte[] bytes) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		byte[] checksum = md.digest(bytes);
+
+		StringBuilder sb = new StringBuilder();
+		for(byte b : checksum) {
+			sb.append(String.format("%02x", b));
+		}
+		return sb.toString();
 	}
 }
