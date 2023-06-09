24ee9487421be1b61991d34eb766126f3286b875
NebelNidas <nebelnidas@gmail.com>
2023-06-09 15:02:45 +0200

    Remove `combineIntervals`, as it undermines the config

--- silence-remover-core/src/main/java/silenceremover/jobs/ProcessIntervalsJob.java
+++ silence-remover-core/src/main/java/silenceremover/jobs/ProcessIntervalsJob.java
@@ -1,11 +1,9 @@
 package silenceremover.jobs;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.DoubleConsumer;
 
 import job4j.Job;
-import job4j.JobState;
 
 import silenceremover.Interval;
 import silenceremover.config.ProjectConfig;
@@ -23,39 +21,8 @@ public class ProcessIntervalsJob extends Job<Void> {
 
 	@Override
 	protected Void execute(DoubleConsumer progressReceiver) throws Exception {
-		combineIntervals(0.3);
-		progressReceiver.accept(0.5);
-		if (state == JobState.CANCELING) return null;
-
 		addPaddingToAudibleSegments();
 		return null;
-	}
-
-	private void combineIntervals(double shortIntervalThreshold) {
-		List<Interval> combinedIntervals = new ArrayList<>();
-		Interval combinedInterval = new Interval(0);
-
-		for (Interval interval : intervals) {
-			if (interval.getDuration() <= shortIntervalThreshold || combinedInterval.isSilent() == interval.isSilent()) {
-				combinedInterval.setEnd(interval.getEnd());
-			} else {
-				if (combinedInterval.isSilent() == null) {
-					combinedInterval.setSilent(interval.isSilent());
-					combinedInterval.setEnd(interval.getEnd());
-				} else {
-					combinedIntervals.add(combinedInterval);
-					combinedInterval = interval.copy();
-				}
-			}
-		}
-
-		if (combinedInterval.isSilent()) {
-			combinedInterval.setSilent(false);
-		}
-
-		combinedIntervals.add(combinedInterval);
-		intervals.clear();
-		intervals.addAll(combinedIntervals);
 	}
 
 	private void addPaddingToAudibleSegments() {
